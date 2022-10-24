d059f56c4c6a9aeb2afb54b995d0951b5fa2c4d1
Julien Atanassian <julien.atanassian@gmail.com>
2022-10-24 15:25:26 -0400

    clean up

--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -1,10 +1,3 @@
-import { useEffect } from "react";
-import { getRedirectResult } from "firebase/auth";
-import {
-  auth,
-  signInWithGooglePopup,
-  createUserDocumentFromAuth,
-} from "../../utils/firebase/firebase.utils";
 import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
 import SignInForm from "../../components/sign-in-form/sign-in-form.component";
 
