eb46e0e62098eb3bfc9f1c1714f78a356d716899
Julien Atanassian <julien.atanassian@gmail.com>
2022-10-28 13:35:07 -0400

    create cart dropdown component

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -2,6 +2,7 @@ import { Fragment, useContext } from "react";
 import { Outlet, Link } from "react-router-dom";
 
 import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 import { UserContext } from "../../contexts/user.context";
 
 import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
@@ -39,6 +40,7 @@ const Navigation = () => {
 
           <CartIcon />
         </div>
+        <CartDropdown />
       </nav>
       <Outlet />
     </Fragment>
