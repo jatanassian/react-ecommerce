cb8848a2fa1d8d58f264742fe09fd67eab666860
Julien Atanassian <julien.atanassian@gmail.com>
2022-11-03 10:20:00 -0400

    add multiple product categories to firebase

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -1,13 +1,22 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
-import PRODUCTS from "../shop-data.json";
+// import { addCollectionAndDocuments } from "../utils/firebase/firebase.utils.js";
+// import SHOP_DATA from "../shop-data.js";
 
 export const ProductsContext = createContext({
   products: [],
 });
 
 export const ProductsProvider = ({ children }) => {
-  const [products, setProducts] = useState(PRODUCTS);
+  const [products, setProducts] = useState([]);
+
+  // // CREATE THE COLLECTIONS/DOCUMENTS IN FIREBASE FOR THE PRODUCTS/CATEGORIES.
+  // // UNCOMMENT ONLY IF REVAMP OF THE PRODUCTS/CATEGORIES
+  // // RUN IT ONCE, THEN COMMENT AGAIN
+  // useEffect(() => {
+  //   addCollectionAndDocuments("categories", SHOP_DATA);
+  // }, []);
+
   const value = { products };
 
   return (
