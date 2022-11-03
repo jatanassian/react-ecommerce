aa13df253bc430045de1207aa9366d26beeb7136
Julien Atanassian <julien.atanassian@gmail.com>
2022-11-03 10:38:07 -0400

    add function to get categories into an object

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -1,7 +1,6 @@
 import { createContext, useState, useEffect } from "react";
 
-// import { addCollectionAndDocuments } from "../utils/firebase/firebase.utils.js";
-// import SHOP_DATA from "../shop-data.js";
+import { getCategoriesAndDocuments } from "../utils/firebase/firebase.utils.js";
 
 export const ProductsContext = createContext({
   products: [],
@@ -12,10 +11,18 @@ export const ProductsProvider = ({ children }) => {
 
   // // CREATE THE COLLECTIONS/DOCUMENTS IN FIREBASE FOR THE PRODUCTS/CATEGORIES.
   // // UNCOMMENT ONLY IF REVAMP OF THE PRODUCTS/CATEGORIES
-  // // RUN IT ONCE, THEN COMMENT AGAIN
+  // // ADD THE IMPORT STATEMENTS, RUN IT ONCE, THEN COMMENT AGAIN
   // useEffect(() => {
   //   addCollectionAndDocuments("categories", SHOP_DATA);
   // }, []);
+
+  useEffect(() => {
+    const getCategoriesMap = async () => {
+      const categoryMap = await getCategoriesAndDocuments();
+      console.log(categoryMap);
+    };
+    getCategoriesMap();
+  }, []);
 
   const value = { products };
 
