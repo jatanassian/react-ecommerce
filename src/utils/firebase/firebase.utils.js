34cc08f6fa54aa13c2bb482ba7d7d2e408bfad82
Julien Atanassian <julien.atanassian@gmail.com>
2022-11-08 11:50:13 -0500

    add log statement for testing purpose

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -28,6 +28,8 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
+console.log(firebaseConfig);
+
 initializeApp(firebaseConfig);
 
 const googleProvider = new GoogleAuthProvider();
