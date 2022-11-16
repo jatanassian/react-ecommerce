5bc28d0cb5274af9b918d4d19e28ece3b66184a0
Julien Atanassian <julien.atanassian@gmail.com>
2022-11-16 16:11:35 -0500

    prevent unwanted selector triggering

--- src/store/categories/categories.selector.js
+++ src/store/categories/categories.selector.js
@@ -1,7 +1,20 @@
-export const selectCategoriesMap = (state) => {
-  return state.categories.categories.reduce((acc, category) => {
-    const { title, items } = category;
-    acc[title.toLowerCase()] = items;
-    return acc;
-  }, {});
-};
+import { createSelector } from "reselect";
+
+const selectCategoryReducer = (state) => state.categories;
+
+export const selectCategories = createSelector(
+  [selectCategoryReducer],
+  (stateCategories) => stateCategories.categories
+);
+
+export const selectCategoriesMap = createSelector(
+  [selectCategories],
+  (categories) => {
+    console.log("selector fired");
+    return categories.reduce((acc, category) => {
+      const { title, items } = category;
+      acc[title.toLowerCase()] = items;
+      return acc;
+    }, {});
+  }
+);
