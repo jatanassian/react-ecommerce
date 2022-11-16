6a5928ed3fcdc6b59e78763742d40cb7258c0d62
Julien Atanassian <julien.atanassian@gmail.com>
2022-11-16 11:24:21 -0500

    directly return map

--- src/store/categories/categories.selector.js
+++ src/store/categories/categories.selector.js
@@ -1,9 +1,7 @@
 export const selectCategoriesMap = (state) => {
-  const categoriesMap = state.categories.categories.reduce((acc, category) => {
+  return state.categories.categories.reduce((acc, category) => {
     const { title, items } = category;
     acc[title.toLowerCase()] = items;
     return acc;
   }, {});
-
-  return categoriesMap;
 };
