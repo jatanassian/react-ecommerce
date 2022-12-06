458a4252dbf84907980bff355603b87a19a1a0f1
Julien Atanassian <julien.atanassian@gmail.com>
2022-12-05 22:43:16 -0500

    turn check user session into saga

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import { Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
 import Home from "./routes/home/home.component";
 import Navigation from "./routes/navigation/navigation.component";
 import Authentication from "./routes/authentication/authentication.component";
 import Shop from "./routes/shop/shop.component";
 import Checkout from "./routes/checkout/checkout.component";
-import { useEffect } from "react";
-import { getCurrentUser } from "./utils/firebase/firebase.utils";
 import { useDispatch } from "react-redux";
+import { checkUserSession } from "./store/user/user.action";
 
 const App = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    getCurrentUser().then((user) => console.log(user));
+    dispatch(checkUserSession());
   }, [dispatch]);
 
   return (
