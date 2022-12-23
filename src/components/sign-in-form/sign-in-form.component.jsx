6851820bad7965f75139b1fdd24eec0c2b134b8b
Julien Atanassian <julien.atanassian@app8pay.com>
2022-12-23 18:37:35 -0500

    fix typo

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -3,7 +3,7 @@ import { useDispatch } from "react-redux";
 import FormInput from "../form-input/form-input.component";
 import Button, { BUTTON_TYPE_CLASSES } from "../button/button.component";
 import { signInAuthUserWithEmailAndPassword } from "../../utils/firebase/firebase.utils";
-import { googleSIgnInStart } from "../../store/user/user.action";
+import { googleSignInStart } from "../../store/user/user.action";
 import { SignInContainer, ButtonsContainer } from "./sign-in-form.styles.jsx";
 
 const defaultFormFields = {
@@ -43,7 +43,7 @@ const SignInForm = () => {
 
   const signInWithGoogle = async (event) => {
     event.preventDefault();
-    dispatch(googleSIgnInStart());
+    dispatch(googleSignInStart());
   };
 
   const resetFormFields = () => {
