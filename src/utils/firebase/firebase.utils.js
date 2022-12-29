bedd8371dc53bcb7855ecbaee7b2d503b01fa3cb
Julien Atanassian <julien.atanassian@gmail.com>
2022-12-28 22:23:34 -0500

    fix sign in not working

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -92,7 +92,7 @@ export const createUserDocumentFromAuth = async (
     }
   }
 
-  return userDocRef;
+  return userSnapshot;
 };
 
 export const createAuthUserWithEmailAndPassword = async (email, password) => {
