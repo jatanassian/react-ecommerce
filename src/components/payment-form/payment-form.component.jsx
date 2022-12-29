b6e9fbdcd47aeaf92c1729bb621997ee63b63a6b
Julien Atanassian <julien.atanassian@gmail.com>
2022-12-29 15:42:43 -0500

    start implementing paymentHandler function

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -1,9 +1,18 @@
-import { CardElement } from "@stripe/react-stripe-js";
+import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
 
 import Button, { BUTTON_TYPE_CLASSES } from "../button/button.component";
 import { PaymentFormContainer, FormContainer } from "./payment-form.styles";
 
 const PaymentForm = () => {
+	const stripe = useStripe();
+	const elements = useElements();
+
+	const paymentHandler = async (event) => {
+		event.preventDefault();
+
+		if (!stripe || !elements) return;
+	}
+
 	return (
 		<PaymentFormContainer>
 			<FormContainer>
