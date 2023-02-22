69ac8b39fbec82dc048dc2f68a6bea1778a052ce
Julien Atanassian <julien.atanassian@gmail.com>
2023-02-22 16:50:15 -0500

    type form input component

--- src/components/form-input/form-input.styles.tsx
+++ src/components/form-input/form-input.styles.tsx
@@ -8,7 +8,11 @@ const shrinkLabelStyles = css`
   color: ${mainColor};
 `;
 
-export const Label = styled.label`
+type FormInputLabelProps = {
+	shrink?: boolean;
+}
+
+export const Label = styled.label<FormInputLabelProps>`
   color: ${subColor};
   font-size: 16px;
   font-weight: normal;
