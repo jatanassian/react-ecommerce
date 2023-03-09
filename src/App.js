09fc8c5d77444795b27aad108861441620329ca8
Julien Atanassian <julien.atanassian@gmail.com>
2023-03-09 11:26:05 -0500

    dynamically import other routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import { useEffect, lazy, Suspense } from "react";
-import Navigation from "./routes/navigation/navigation.component";
-import Authentication from "./routes/authentication/authentication.component";
-import Shop from "./routes/shop/shop.component";
-import Checkout from "./routes/checkout/checkout.component";
 import { useDispatch } from "react-redux";
 import Spinner from "./components/spinner/spinner.component";
 import { checkUserSession } from "./store/user/user.action";
 
 const Home = lazy(() => import('./routes/home/home.component'));
+const Authentication = lazy(() => import('./routes/authentication/authentication.component'));
+const Shop = lazy(() => import('./routes/shop/shop.component'));
+const Checkout = lazy(() => import('./routes/checkout/checkout.component'));
+const Navigation = lazy(() => import('./routes/navigation/navigation.component'));
 
 const App = () => {
   const dispatch = useDispatch();
